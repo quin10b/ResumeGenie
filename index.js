c44549018cfdc176207bb84d80ebce7ce534b3d5
QtipSchool <qtipschool@Robertas-MacBook-Pro.local>
2024-03-24 00:27:16 -0400

    TEST

---
+++ index.js
@@ -0,0 +1,11 @@
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+});
+
+const chatCompletion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "user", "content": "Hello!"}],
+  });
+  console.log(chatCompletion.choices[0].message);
