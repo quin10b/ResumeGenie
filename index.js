a3a0ed035936899bab4e9665b46d022a81112b2a
QtipSchool <qtipschool@Robertas-MacBook-Pro.local>
2024-03-24 03:43:56 -0400

    added script start

--- index.js
+++ index.js
@@ -45,3 +45,10 @@ app.listen(port,()=>{
     // do not add localhost here if you are deploying it
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
