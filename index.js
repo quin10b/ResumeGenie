6666b9abdfa96bc1afbbb4493fd7c3a1116450cb
QtipSchool <qtipschool@Robertas-MacBook-Pro.local>
2024-03-24 01:24:05 -0400

    able to run after running the local host

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
+//allows retrieval of APIKey from .env file
 dotenv.config();
 
 const openai = new OpenAI({
+    
     apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
   });
 
-const chatCompletion = await openai.chat.completions.create({
-  model: "gpt-3.5-turbo",
-  messages: [{"role": "user", "content": "Hello!"}],
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/",async (req,res) => {
+
+    const {message} = req.body
+    const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        
+        messages: [
+            {"role": "system", "content": "You are QuinBOT"},
+            {role: "user", content: `${message}`}],
+      });
+
+    res.json({
+        completion: completion.choices[0].message
+    })
+
+
 });
-console.log(chatCompletion.choices[0].message);
+
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
