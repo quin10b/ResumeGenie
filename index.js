85b60fd920363233fb9e45716b7c6042628fa633
QtipSchool <qtipschool@Robertas-MacBook-Pro.local>
2024-03-24 00:50:01 -0400

    able to print api output

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import OpenAI from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
-});
+    apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  });
 
 const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{"role": "user", "content": "Hello!"}],
-  });
-  console.log(chatCompletion.choices[0].message);
+  model: "gpt-3.5-turbo",
+  messages: [{"role": "user", "content": "Hello!"}],
+});
+console.log(chatCompletion.choices[0].message);
