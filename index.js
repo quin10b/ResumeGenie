f9e6a51214a8fe86a17a1e3ff27d6f9af5636279
QtipSchool <qtipschool@Robertas-MacBook-Pro.local>
2024-03-24 04:12:05 -0400

    added script start

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'front_end', 'build')));
+  app.use(express.static(path.join(__dirname)));
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
   });
 }
 
