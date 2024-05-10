410544683fc4e81c6f5c78fc1b9f0459fa063d03
QtipSchool <qtipschool@wireless-10-104-93-200.umd.edu>
2024-05-10 19:31:34 -0400

    port change

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const openai = new OpenAI({
   });
 
 const app = express();
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
