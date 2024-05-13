9db1e9007ed938d297d11080aa65638d93c1bb7c
QtipSchool <qtipschool@wireless-10-104-93-200.umd.edu>
2024-05-13 03:13:01 -0400

    added login function

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ import cors from 'cors';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import readline from 'readline';
-const app = express();
-const port = 3000;
 
-app.set("view engine", "ejs");
-app.set("views", path.join("/Users/qtipschool/Desktop/QuinBOT", "template"));
+import bcrypt from 'bcrypt';
+
+
+import { MongoClient } from 'mongodb';
+import { ServerApiVersion } from 'mongodb';
+const uri = `mongodb+srv://quin10b:Cooldudes12$@cluster0.413gp0l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const databaseAndCollection = {db: "LoginCredentials", collection:"LoginCollection"};
+
 
 
 //allows retrieval of APIKey from .env file
@@ -22,14 +30,22 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
   });
 
+const app = express();
+const port = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'template'));
 
 app.use(bodyParser.json());
 app.use(cors());
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname));
 
 
 const rl = readline.createInterface({
@@ -52,17 +68,11 @@ rl.on('line', (input) => {
   }
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname)));
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
-}
-  
-app.get("/", (request, response) => { 
-  response.render("index");
+app.get("/", (req, res) => { 
+  res.render("index1");
 });
+
 
 app.post("/",async (req,res) => {
 
@@ -85,6 +95,46 @@ app.post("/",async (req,res) => {
     })
 
 });
+
+app.post("/createAccount", (req, res) => { 
+  
+  const login = {
+      email: req.body.email,
+      password: req.body.password
+  }
+  addAccount(client, databaseAndCollection, login);
+  res.render('index1');
+});
+
+
+app.post('/login', async (req, res) => {
+  await client.connect()
+  let filter = {email: req.body.email};
+  const user = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).findOne(filter);
+  await client.close();
+
+  if (user) {
+    const result = req.body.password === user.password;
+    if (result) {
+      res.render("index1");
+    } else {
+      res.status(400).json({ error: "password doesn't match" });
+    }
+  }
+  else{
+    res.status(400).json({ error: "User doesn't exist" });
+  }
+
+  res.render('index1');
+});
+
+
+
+async function addAccount(client, databaseAndCollection, login) {
+  await client.connect();
+  const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(login);
+  await client.close();
+}
 
 console.log(openai.apiKey)
 process.stdout.write(`Web server running at http://localhost:${port}\n`);
